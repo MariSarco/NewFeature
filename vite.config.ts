c14aadf960267f6ab7dabcbe05adbeb773c19b34
MariSarco <seunsteam@mail.ru>
2024-12-11 16:34:58 +0700

    feat: rework

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
       },
     }),
   ],
-  base: process.env.NODE_ENV === 'production' ? '/NewFeature/' : '/',
+  base: process.env.NODE_ENV === "production" ? "/NewFeature/" : "/",
 });
